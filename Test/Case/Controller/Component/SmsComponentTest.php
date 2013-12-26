d1ada4ce37a4c462d4023134ab423a3f3c93ad22
tranfuga25s <tranfuga25s@gmail.com>
2013-12-26 18:21:40 -0300

    Mejorados metodos de pruebas de configuracion

--- Test/Case/Controller/Component/SmsComponentTest.php
+++ Test/Case/Controller/Component/SmsComponentTest.php
@@ -16,6 +16,11 @@ class SmsComponentTest extends CakeTestCase {
 
     private $controlador = null;
 
+    private $request_code = 'TSSFE';
+    private $client_id = 200;
+    private $key = '83978016f41d43314766b7116f922284';
+    private $method = 'GET';
+
     /**
      * setUp method
      *
@@ -27,6 +32,7 @@ class SmsComponentTest extends CakeTestCase {
         $this->Sms = new SmsComponent( $this->Collection );
         $this->Session = new SessionComponent( $this->Collection );
         $this->controlador = new AppController();
+        $this->Sms->startup($this->controlador);
     }
 
     /**
@@ -43,8 +49,26 @@ class SmsComponentTest extends CakeTestCase {
         parent::tearDown();
     }
 
-    public function testbasic() {
-        $this->assertEqual( true, true );
+    /*!
+     * Pruebo que existan los archivos de configuracion
+     */
+    public function testInicializacion() {
+        $this->Sms->initialize( $this->controlador );
+        $this->assertNotEqual( $this->Sms->getClientId(), 0, "La clave de cliente no puede ser cero" );
+        $this->assertNotEqual( $this->Sms->getKey(), null, "La clave de cliente no puede ser nula" );
+        $this->assertNotEqual( $this->Sms->getMethod(), null, "El metodo no puede ser nulo" );
+        $this->assertNotEqual( $this->Sms->getUrl(), null, "El Url no puede ser nulo" );
+        $this->assertNotEqual( $this->Sms->getRequestCode(), null                      , "El Request code no coincide" );
+    }
+
+    public function testCodigosDevoluciones() {
+        /// @TODO: Agregar validaciones
+        /* $this->assertEqual( $this->Sms->devolucionCorrecta(), "OK", "La devolucion correcta debe ser la cadena OK" );
+        $this->assertEqual( $this->Sms->devolucionIncorrecta(), "ERROR", "La devolución incorrecta debe ser la cadena ERROR" );*/
+    }
+
+    public function testHabilitado() {
+        $this->assertEqual( $this->Sms->habilitado(), true, "El componente no está habilitado" );
     }
 
 }
